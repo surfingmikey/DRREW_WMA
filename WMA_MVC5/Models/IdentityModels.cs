280de9d8ccc3e4058df4b8c7c68b573eb6e2d344
surfingmikey <mikey.bitfishy@gmail.com>
2013-12-30 08:07:05 +1000

    including the account classes.

--- WMA_MVC5/Models/IdentityModels.cs
+++ WMA_MVC5/Models/IdentityModels.cs
@@ -1,17 +1,91 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
-namespace WMA_MVC5.Models
+namespace AspNetRoleBasedSecurity.Models
 {
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public string ElispeUser { get; set; }
+        
+      
+        [StringLength(100, ErrorMessage =
+            "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string ElispePassword { get; set; }
+
+        public string District { get; set; }
+        public string Wharehouse { get; set; }
     }
+
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("WMA")
+            : base("DefaultConnection")
         {
+
+        }
+    }
+
+
+    public class IdentityManager
+    {
+        public bool RoleExists(string name)
+        {
+            var rm = new RoleManager<IdentityRole>(
+                new RoleStore<IdentityRole>(new ApplicationDbContext()));
+            return rm.RoleExists(name);
+        }
+
+
+        public bool CreateRole(string name)
+        {
+            var rm = new RoleManager<IdentityRole>(
+                new RoleStore<IdentityRole>(new ApplicationDbContext()));
+            var idResult = rm.Create(new IdentityRole(name));
+            return idResult.Succeeded;
+        }
+
+
+        public bool CreateUser(ApplicationUser user, string password)
+        {
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var idResult = um.Create(user, password);
+            return idResult.Succeeded;
+        }
+
+
+        public bool AddUserToRole(string userId, string roleName)
+        {
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var idResult = um.AddToRole(userId, roleName);
+            return idResult.Succeeded;
+        }
+
+
+        public void ClearUserRoles(string userId)
+        {
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var user = um.FindById(userId);
+            var currentRoles = new List<IdentityUserRole>();
+            currentRoles.AddRange(user.Roles);
+            foreach (var role in currentRoles)
+            {
+                um.RemoveFromRole(userId, role.Role.Name);
+            }
         }
     }
 }
